81c40fea973fa6559ae3df5857acd241da0c6b21
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-08 17:11:52 +0000

    00 to 09 are now numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print('0{:d}'.format(i), end='')
+        print('{:d}{:d}'.format(0, i), end='')
     else:
         print('{:d}'.format(i), end='')
     if i != 99:
