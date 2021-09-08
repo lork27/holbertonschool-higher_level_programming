e007bd33c1cf7937a8490f4eae0ce9021c63af53
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-08 18:04:43 +0000

    fixed ouput styling

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,9 @@ def print_last_digit(number):
     if number < 0:
         n = number % -10
         n = abs(n)
-        print('{:d}'.format(n))
+        print('{:d}'.format(n), end='')
         return(n)
     else:
         n = number % 10
-        print('{:d}'.format(n))
+        print('{:d}'.format(n), end='')
         return(n)
