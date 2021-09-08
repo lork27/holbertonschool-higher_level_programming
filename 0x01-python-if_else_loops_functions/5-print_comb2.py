c7fa75cf33f6d51e80fecdde329540f7b217f528
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-08 17:23:03 +0000

    ADD {:02d} format specifier

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print('{:d}{:d}'.format(0, i), end='')
+    if i < 99:
+        print('{:02d}, '.format(i), end='')
     else:
-        print('{:d}'.format(i), end='')
-    if i != 99:
-        print(end=', ')
-    else:
-        print()
+        print('{:d}'.format(i))
