82e666db13be7167f9c14b73d385d837fd5cbbfe
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-08 17:05:18 +0000

    changed loop to end at 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 101):
+for i in range(0, 100):
     if i < 10:
         print('0{:d}'.format(i), end='')
     else:
         print('{:d}'.format(i), end='')
-    if i != 100:
+    if i != 99:
         print(end=', ')
     else:
         print()
