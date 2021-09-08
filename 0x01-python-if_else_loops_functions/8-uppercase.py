591ac92dc7dbe06184b4134c46eb2ff66d7eca4f
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-08 17:49:20 +0000

    change str var name

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    upper = ""
+    upstr = ""
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            upper += chr(ord(i) - 32)
+            upstr += chr(ord(i) - 32)
         else:
-            upper += i
-    print('{}'.format(upper))
+            upstr += i
+    print('{}'.format(upstr))
