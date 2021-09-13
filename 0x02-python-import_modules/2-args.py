fd728d33648bf8f9bc676c4a4d7508443fa96bab
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-13 17:27:48 +0000

    PRINTS number and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 import sys
 length = len(sys.argv)
 if len(sys.argv) <= 1:
-    print("{:d} arguments.".format(length))
+    print("{} arguments.".format(0))
 else:
-    print("{:d} arguments:".format(length - 1))
+    print("{} arguments:".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format(i, sys.argv[i]))
