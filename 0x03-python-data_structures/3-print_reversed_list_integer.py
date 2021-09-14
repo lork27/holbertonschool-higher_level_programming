b8df18b54bef6794c305df46bcffc99ad4c6a1c1
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-14 17:09:32 +0000

    ADDED edgecase for no list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
-        return
+        return None
     else:
         for length in range(len(my_list) - 1, - 1, - 1):
             print("{:d}".format(my_list[length]))
