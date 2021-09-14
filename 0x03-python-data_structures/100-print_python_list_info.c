1a5016aee5793d15e88a6ae4926644385942584e
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-14 22:52:16 +0000

    testing something

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
+
+
+void print_python_list_info(PyObject *p);
+
+int main(void)
+{
+	static PhyObject *method_fputs(PyObject *self, PyObject *args)
+	{
+		char *str, *filename = NULL;
+		int bytes_copied = -1;
+		void print_python_list_info(PyCfuntion);
+	}
+	void print_python_list_info(method_fputs);
+
+}
+
+
+void print_python_list_info(PyObject *p)
+{
+
+}
