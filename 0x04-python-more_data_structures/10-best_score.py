119c5362e7c820bfc4143993344e9725291c632e
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-16 23:13:22 +0000

    RETURNS key with the biggest int value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,4 +4,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return list(sorted(a_dictionary.keys(), reverse=True))[0]
+    return sorted(a_dictionary, key=a_dictionary.get)[-1]
