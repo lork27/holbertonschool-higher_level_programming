23b76d24406b81627ed7e91bd6ac902f4cc58276
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-17 01:52:14 +0000

    returns weigthed average of ins in tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,12 +3,9 @@
 
 def weight_average(my_list=[]):
 
-    if not my_list:
-        return 0
-
     total = 0
     divisor = 0
     for i in my_list:
-        total += (i[0] * i[1])
+        total += i[0] * i[1]
         divisor += i[1]
     return total / divisor
