84093cf65a914c2e391f03ea937b7767d97214da
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-27 19:34:05 +0000

    prints integers in a safe way

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
-        print("Exception: Unknown format code 'd' for object type 'str'", file=sys.stderr)
+    except Exception as e:
+        print("Exception:", e)
         return False
