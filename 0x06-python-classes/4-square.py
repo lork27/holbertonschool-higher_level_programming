26a8c20431f89551c53eb90863a8f1dc6d3cb95e
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-28 20:32:11 +0000

    getter and setter practise

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module contains Square class"""
+
+
+class Square:
+    """contains size attribute"""
+    def __init__(self, size=0):
+        """init first instance of class
+        args:
+            size: size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
