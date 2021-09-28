1fd5c319f837ea52dd416098a62e7449bf7e0098
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-28 23:21:05 +0000

    object that can prints square out of int and tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,15 +18,17 @@ class Square:
 
     @position.setter
     def position(self, value):
+        s = "position must be a tuple of 2 positive integers"
         if isinstance(value[0], int) is False:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if isinstance(value[1], int) is False:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value) is not tuple:
-            raise TypeError("size must be an integer")
-        self._position = value
+            raise TypeError(s)
+        elif isinstance(value[1], int) is False:
+            raise TypeError(s)
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError(s)
+        elif type(value) is not tuple:
+            raise TypeError(s)
+        else:
+            self._position = value
 
     @property
     def size(self):
