473f943c86bbf9c22f8850218315297e49f5f729
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-04 23:00:08 +0000

    fixed multiple typos

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """width setter"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,8 +34,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
