7db37384e63bea192c3831795f52eb04f3e0a99b
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-04 15:41:11 +0000

    class rectangle that defines width and heigh

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""module that contains rectangle class"""
+
+
+class Rectangle:
+    """class containts """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = width
+
+    """ width getter and setter """
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """ height getter and setter """
+
+    @property
+    def height(self):
+        """height getter"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
