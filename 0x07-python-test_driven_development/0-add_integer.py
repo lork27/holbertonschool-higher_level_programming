543ca2fc931ef372c2fe43a1a52413cec67e4279
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-05 15:29:28 +0000

    fixed pep8 linter

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,17 +3,9 @@
 
 
 def add_integer(a, b=98):
-    """add_integer: functions that adds two numbers
-    parameters:
-        a: first integer
-        b: second integer, its 98 if nothing is passed
-    Raised:
-        TypeError: if a or b not int/float
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
 
     return int(a) + int(b)
