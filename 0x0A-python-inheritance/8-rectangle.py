c3c806d7ba324b527408f867ede59679d66605a2
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-12 22:57:24 +0000

    removed class declaration and changed it into an import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-'''module that contains BaseGeometry class
+'''module that imports BaseGeometry class
 and Rectangle child class
 '''
 
 
-class BaseGeometry():
-    '''class contains attributes and methods'''
-    def area(self):
-        """method that defines area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates input"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
