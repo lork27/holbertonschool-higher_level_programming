543c058551d11d587566fcc7aef9c6bc5737f312
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-12 17:34:24 +0000

    override of __str__ method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''module that contains BaseGeometry class
+and Rectangle child class
+'''
+
+
+class BaseGeometry():
+    '''class contains attributes and methods'''
+    def area(self):
+        """method that defines area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method that validates input"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """method that initializes class"""
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """overrides parent area method"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """__str__ of rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """class that inherits from rectangle"""
+    def __init__(self, size):
+        """initializer of Square class"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """overrrides area method of parent class"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """__str__ of square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
