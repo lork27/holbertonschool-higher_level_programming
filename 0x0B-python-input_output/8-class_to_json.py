542068470ec4fe31d89e87ed94031bb0d3d18317
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-13 22:21:06 +0000

    returns dict description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 '''module that contains class_to_json function'''
-import json
 
 
 def class_to_json(obj):
     '''returns a dictionary description with simple structure data'''
-    return vars(obj)
+    return obj.__dict__('__dict__')
