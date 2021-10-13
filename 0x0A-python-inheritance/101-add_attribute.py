60b52f6a7883373446e1435b588e990e6e0d2790
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-13 02:23:48 +0000

    function that adds attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module that contains a function that adds a new
+attribute to an object if possible"""
+
+
+def add_attribute(obj, name, att):
+    """function that tries to add attribute to class"""
+    if isinstance(obj, object):
+        setattr(obj, name, att)
+    else:
+        raise TypeError("can't add new attribute")
