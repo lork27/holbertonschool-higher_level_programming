7cacafa410f933b99ca2171dfc4d22ffe5b1874a
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-13 22:59:51 +0000

    added method that prints dict based on attributes passed

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''module that contains student module'''
+
+
+class Student:
+    '''student class with first_name, last_name and age'''
+    def __init__(self, first_name, last_name, age):
+        '''instanciates object'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''method that prints dict of student'''
+        if attrs is None:
+            return self.__dict__
+        newdict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                newdict[key] = value
+
+        return newdict
