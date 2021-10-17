21940cba911b256ca1d3e12f40c6c7a8dab23a07
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-17 02:24:18 +0000

    updated rectangles to task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,25 @@ class Rectangle(Base):
         self.y = y
 
     def __str__(self):
+        '''str implementation'''
         s = "[Rectangle] ({}) {}/{} - {}/{}"
         return s.format(self.id, self.x, self.y, self.width, self.height)
 
+    def update(self, *args):
+        '''updates object attributes'''
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def display(self):
+        '''properly displays rectangle'''
         for idx in range(self.y):
             print()
         for i in range(self.height):
