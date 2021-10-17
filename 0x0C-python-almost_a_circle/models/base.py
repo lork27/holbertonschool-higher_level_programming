40a2bb39cbe3ae8fa30236d182ac969def1a9a88
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-17 15:18:37 -0400

    updated base class for task 14-17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''module that contains Base Class'''
+import json
 
 
 class Base:
@@ -13,3 +14,28 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns json representation of list of dictionaries'''
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''returns list of the JSON string'''
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''saves JSON representation of an object to a text file'''
+        if list_objs is None:
+            with open(cls.__name__ + ".json", 'w', encoding="utf-8") as file:
+                file.write("[]")
+        else:
+            list_of_dict = []
+            for obj in list_objs:
+                list_of_dict.append(obj.to_dictionary())
+            with open(cls.__name__ + ".json", 'w', encoding="utf-8") as file:
+                file.write(Base.to_json_string(list_of_dict))
