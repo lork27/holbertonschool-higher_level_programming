f1cc4abb1db90392bc2b485e26a7204ca86fd755
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-19 17:36:18 +0000

    made preparations for the cvs taks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,3 +67,31 @@ class Base:
                 return list_objs
         except IOError:
             return []
+
+    '''cvs methods'''
+    @classmethod
+    def save_to_file_cvs(cls, list_objs):
+        '''saves CVS representation of an object to a text file'''
+        if list_objs is None:
+            with open(cls.__name__ + ".cvs", 'w', encoding="utf-8") as file:
+                file.write("[]")
+        else:
+            list_of_dict = []
+            for obj in list_objs:
+                list_of_dict.append(obj.to_dictionary())
+            with open(cls.__name__ + ".cvs", 'w', encoding="utf-8") as file:
+                file.write(Base.to_json_string(list_of_dict))
+
+    @classmethod
+    def load_from_file(cls):
+        '''returns a list of instances from a cvs file'''
+        filename = cls.__name__ + ".cvs"
+        list_objs = []
+        try:
+            with open(filename, 'r', encoding="utf-8") as f:
+                list_instance = Base.from_json_string(f.read())
+                for dict in list_instance:
+                    list_objs.append((cls.create(**dict)))
+                return list_objs
+        except IOError:
+            return []
