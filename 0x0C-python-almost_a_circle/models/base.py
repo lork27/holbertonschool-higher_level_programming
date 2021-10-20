01dc3b4ba809a2117086060569f769c6ab8e8994
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-20 00:31:57 +0000

    file_to_csv now works

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''module that contains Base Class'''
 import json
+import csv
 
 
 class Base:
@@ -68,30 +69,22 @@ class Base:
         except IOError:
             return []
 
-    '''cvs methods'''
     @classmethod
-    def save_to_file_cvs(cls, list_objs):
-        '''saves CVS representation of an object to a text file'''
+    def save_to_file_csv(cls, list_objs):
+        '''saves CSV representation of an object to a text file'''
         if list_objs is None:
-            with open(cls.__name__ + ".cvs", 'w', encoding="utf-8") as file:
+            with open(cls.__name__ + ".csv", 'w', encoding="utf-8") as file:
                 file.write("[]")
         else:
+            if cls.__name__ == "Rectangle":
+                header = ['id', 'width', 'height', 'x', 'y']
+            else:
+                header = ['id', 'size', 'x', 'y']
             list_of_dict = []
             for obj in list_objs:
                 list_of_dict.append(obj.to_dictionary())
-            with open(cls.__name__ + ".cvs", 'w', encoding="utf-8") as file:
-                file.write(Base.to_json_string(list_of_dict))
-
-    @classmethod
-    def load_from_file_cvs(cls):
-        '''returns a list of instances from a cvs file'''
-        filename = cls.__name__ + ".cvs"
-        list_objs = []
-        try:
-            with open(filename, 'r', encoding="utf-8") as f:
-                list_instance = Base.from_json_string(f.read())
-                for dict in list_instance:
-                    list_objs.append((cls.create(**dict)))
-                return list_objs
-        except IOError:
-            return []
+            with open(cls.__name__ + ".csv", 'w', encoding="utf-8") as file:
+                wr = csv.DictWriter(file, fieldnames=header)
+                wr.writeheader()
+                for dic in list_of_dict:
+                    wr.writerow(dic)
