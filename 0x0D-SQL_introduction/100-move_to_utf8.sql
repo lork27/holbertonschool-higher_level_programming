b457752adc7815d7156bc57101c0c4c379b5758c
lork27 <g.lorca.lamadrid@gmail.com>
2021-11-16 21:59:05 +0000

    convert to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- deletes database
-ALTER DATABASE htbn_0C_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0C_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER name FROM first_table TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
