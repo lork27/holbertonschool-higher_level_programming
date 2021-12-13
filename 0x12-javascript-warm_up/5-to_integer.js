58f5e979d63cd9f2d33e9a116e0e7da762deeb12
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-13 18:01:40 -0400

    fixed typo

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const argInt = parseInt((process.argv[2]));
 if (isNaN(argInt)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', argInt);
+  console.log('My number:', argInt);
 }
