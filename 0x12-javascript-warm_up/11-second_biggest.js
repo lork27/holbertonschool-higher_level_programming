c340e61ae1cd72ca7ec7bb56bb7f082ac445f438
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-13 16:29:56 -0400

    returns second largest number of argv

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('process');
+let { argv } = require('process');
 const process = require('process');
 const argA = parseInt((process.argv[2]));
 // let argList = [];
@@ -7,13 +7,6 @@ const argA = parseInt((process.argv[2]));
 if (argv.length === 2 || argA === 1) {
   console.log(1);
 } else {
-  // let i = 2;
-  // let x = 0;
-  // while (i < argv.length) {
-  //   argList[x] = parseInt(argv[i]);
-  //   i++;
-  //   x++;
-  // }
-  // console.log(Math.max(argList));
-  console.log(argv);
-  }
+  argv = argv.sort((a, b) => a - b);
+  console.log(argv[argv.length - 2]);
+}
