596623ce32a48b8e298e22bbbbc02195f42377ad
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-13 17:37:16 -0400

    adds method to object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr: function() { this.value = this.value + 1; }
 };
 console.log(myObject);
-myObject.incr = () => { this.value = this.value + 1; };
+// myObject.incr = () => { this.value = this.value + 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
