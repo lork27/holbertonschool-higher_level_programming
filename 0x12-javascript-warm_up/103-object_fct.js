34114f092a34b10f114cc36ecddfdf8e8d96bc26
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-13 17:58:43 -0400

    used shorthand for this.value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,8 +3,8 @@ const myObject = {
   type: 'object',
   value: 12
 };
-myObject.incr = function() { this.value = this.value + 1; };
 console.log(myObject);
+myObject.incr = function () { this.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
