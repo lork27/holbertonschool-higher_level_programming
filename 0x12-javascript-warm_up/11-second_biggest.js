ad2ff5b749e662dbde5db2f6f01d8281f351329b
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-13 16:30:46 -0400

    returns second largest number of argv

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const argA = parseInt((process.argv[2]));
 // let argList = [];
 
 if (argv.length === 2 || argA === 1) {
-  console.log(1);
+  console.log(0);
 } else {
   argv = argv.sort((a, b) => a - b);
   console.log(argv[argv.length - 2]);
