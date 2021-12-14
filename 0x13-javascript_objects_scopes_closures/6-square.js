e7848ecf0a5f04a3c427244b7871c78b68676894
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-14 19:53:39 -0400

    test

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,12 +4,11 @@ const BaseSquare = require('./5-square');
 class Square extends BaseSquare {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c = 'X') {
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
       }
       process.stdout.write('\n');
