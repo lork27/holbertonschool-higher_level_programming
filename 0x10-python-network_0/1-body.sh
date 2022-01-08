124937ea6a66058e22c9722675b38ee7e7d94683
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-08 17:22:38 -0400

    display body of the get response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/bash
 #display body of the get response
-curl -s -w "\n%{http_code}" 'https://swapi.co/api/people/1/?format=json' | {
-    read body
-    read code
-    echo $code
-    jq .name <<< "$body"
-}
+curl -s -w "\n%{http_code}" '$1'
