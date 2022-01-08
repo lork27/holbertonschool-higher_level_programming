0700ff87eefaac0d937f32aa1bc99ea29c4118fd
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-08 18:02:56 -0400

    sends POST request and displays body

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #display body of the get response
-curl -X POST -sH 'email:test@gmail.com' -H 'subject:I will always be here for PLD' $1
+curl -X POST -s -H 'email:test@gmail.com' -H 'subject:I will always be here for PLD' $1
