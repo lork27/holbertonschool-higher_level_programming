9d8643f69c85992bb6b2f22d446447bb1ac9f6c8
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-09 17:26:40 -0400

    displays body of request URL and handles exceptions

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == '__main__':
     url = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.reason))
+        print("Error code: {}".format(e.code))
