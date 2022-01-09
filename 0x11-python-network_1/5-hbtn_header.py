3a15ffee36709c9754ded0842d01bb165eb1119f
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-09 17:56:52 -0400

    script that prints variable from header response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,7 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    try:
+        print(r.headers['X-Request-Id'])
+    except:
+        print(None)
