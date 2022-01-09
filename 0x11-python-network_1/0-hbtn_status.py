7e0459b1ecb7febd797266b05f914828fb0e55d3
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-08 20:08:42 -0400

    Python script that fectches url and prints body

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,9 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+        print("""Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(
             type(html), html,
             html.decode('utf8')))
