b5cccd0d7f804453159d33168010773f554abaa5
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-11 12:09:12 -0400

    function that finds a peak in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-'''module that contains function that returns peak element on a list'''
+''' Has a function that finds a peak '''
 
 
 def find_peak(list_of_integers):
+    ''' finds a peak in a list of unsorted integers.'''
     li = list_of_integers
-    leng = len(li)
-    peak = 0
-    if leng == 0:
+    if len(li) == 0:
         return None
-    if leng == 1:
+    if len(li) == 1:
         return
     if li[0] >= li[1]:
         return li[0]
     elif li[-1] >= li[-2]:
         return li[-1]
-    for i in range(1, leng - 1):
-        if li[i] >= li[i - 1] and li[i] >= li[i + 1]:
-            return li[i]
+    for idx in range(1, len(li) - 1):
+        if li[idx] >= li[idx - 1] and li[idx] >= li[idx + 1]:
+            return li[idx]
