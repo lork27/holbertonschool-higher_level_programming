0cfbe4c553a34ac04623d799208ae849528f6c02
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-01 13:10:45 -0400

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 const fs = require('request');
-if (process.argv.length == 3) {
-  const url = process.argv[2];
+const url = process.argv[2];
 
-  fs(url, function (a, response, b) {
-    console.log('Code:', response.statusCode); // Print the response status code if a response was received
-  });
-}
-else {
-  console.log("ayy")
-}
+fs(url, function (a, response, b) {
+  console.log('Code:', response.statusCode); // Print the response status code if a response was received
+});
