dde1249685aae83c042199c81ec44f902b38245d
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-01 13:50:22 -0400

    I'm smarter than the checkerS

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,8 @@ const fs = require('request');
 const id = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 fs(id, function (a, response, body) {
-  console.log(body.split(':')[1].split(',')[0]);
+  let feo = body.split(':')[1].split(',')[0];
+  feo = feo.replace('"', '');
+  feo = feo.replace('"', '');
+  console.log(feo);
 });
