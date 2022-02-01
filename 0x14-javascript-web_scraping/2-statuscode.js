1cd7245d9468afff08e3c1c722c54ecf88fb3b53
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-01 13:22:52 -0400

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const fs = require('request');
 const url = process.argv[2];
 
-fs.get(url).on('response', function(response) {
-  console.log("code: ", response.statusCode)
-})
+fs.get(url).on('response', function (response) {
+  console.log('code: ', response.statusCode);
+});
